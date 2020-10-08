9482434597e0ca1a2bb7ff5a5bdc18d72577a0b1
Guilherme <guilherme.bralves@hotmail.com>
2020-10-08 20:33:48 -0300

    Category adjust

---
+++ src/main/java/com/guisa/myshop/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.guisa.myshop.resources.exception;
+
+import javax.servlet.http.HttpServletMapping;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.guisa.myshop.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletMapping request) {
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+}
---
+++ src/main/java/com/guisa/myshop/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.guisa.myshop.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+	
+
+}
