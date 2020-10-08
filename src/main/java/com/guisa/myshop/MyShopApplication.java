8d2bac08568211a4d20334bef9cb081d0615b6a7
Guilherme <guilherme.bralves@hotmail.com>
2020-10-08 19:59:12 -0300

    Create operation of instaciation

--- src/main/java/com/guisa/myshop/MyShopApplication.java
+++ src/main/java/com/guisa/myshop/MyShopApplication.java
@@ -1,13 +1,32 @@
 package com.guisa.myshop;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.guisa.myshop.domain.Categoria;
+import com.guisa.myshop.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class MyShopApplication {
+public class MyShopApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyShopApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
+
 	}
 
 }
