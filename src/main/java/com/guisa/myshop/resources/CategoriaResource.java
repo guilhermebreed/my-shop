fab6eb337e296556ee32419d055e304c369a1a3c
Guilherme <guilherme.bralves@hotmail.com>
2020-10-08 19:31:21 -0300

    Banco H2 e teste da base de dados

--- src/main/java/com/guisa/myshop/resources/CategoriaResource.java
+++ src/main/java/com/guisa/myshop/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.guisa.myshop.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.guisa.myshop.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST está funcionando";
+	public List<Categoria> list() {
+		
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		
+		return lista;
 	}
 }
